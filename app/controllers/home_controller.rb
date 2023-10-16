0565639f21955eba93dc493875010dd809ff8ed3
amparoq <aquiroz@miuandes.cl>
2023-10-16 19:52:21 -0300

    lalo avance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,11 @@ class HomeController < ApplicationController
     def index
         @groups = current_user.groups 
         @users_in_groups = {}
-        @expired_tasks = 
       
         @groups.each do |group|
           @users_in_groups[group] = group.users
         end
+        @tasks = Task.where(group_id: @groups.pluck(:id),status: 0)
     end
 
 
